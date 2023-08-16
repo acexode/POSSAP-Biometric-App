6f02a83bfc6a02dbbd2e65caabadd22588b96def
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2023-08-16 11:37:37 +0100

    login functionality

--- src/_apis_/auth/login.js
+++ src/_apis_/auth/login.js
@@ -5,20 +5,20 @@ import { computeCBSBody } from "../../utils/computeBody";
 
 export const loginUrl = getUrlString(LOGIN);
 
-import config from '../../config.json'
+import config from "../../config.json";
 export default async function loginUser(data) {
   console.log(data);
   const body = computeCBSBody(
     "post",
-    loginUrl,
+    "https://test.possap.ng/api/v1/pss/proxyauthentication/signin",
     {},
     "",
-    '',
+    "",
     data
   );
-  const configUrl = config.middlewareApi + '/fetch-data'
+  const configUrl = config.cbsRoute + "/fetch-data";
   const response = await axios.post(configUrl, body);
-
+  console.log(response);
   // if (!response.ok) {
   //   throw new Error(`Error occurred while trying to login user`);
   // }
