7436dcb37100d692ddeec5e2ebca0de55583fe71
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2023-08-17 11:04:59 +0100

    login auth updates

--- src/_apis_/auth/login.js
+++ src/_apis_/auth/login.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import getUrlString from "../../utils/get-url-string";
 import { LOGIN } from "../../constants/api-routes";
 import { computeCBSBody } from "../../utils/computeBody";
-
 export const loginUrl = getUrlString(LOGIN);
 
 import config from "../../config.json";
@@ -10,7 +9,7 @@ export default async function loginUser(data) {
   console.log(data);
   const body = computeCBSBody(
     "post",
-    "https://test.possap.ng/api/v1/pss/proxyauthentication/signin",
+    loginUrl,
     {},
     "",
     "",
