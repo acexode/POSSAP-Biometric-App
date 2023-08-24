c580025133c772c7c808b61d9647e3958adae66a
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2023-08-24 10:40:30 +0100

    updates

--- src/_apis_/auth/login.js
+++ src/_apis_/auth/login.js
@@ -3,7 +3,7 @@ import getUrlString from "../../utils/get-url-string";
 import { LOGIN } from "../../constants/api-routes";
 import { computeCBSBody } from "../../utils/computeBody";
 export const loginUrl = getUrlString(LOGIN);
-
+import config from '../../config.json'
 export default async function loginUser(data) {
   console.log(data);
   const body = computeCBSBody(
