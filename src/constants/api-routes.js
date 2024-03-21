ac038caa68da5728de5f2d8818dc31ba36532d97
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2024-03-21 10:45:24 +0100

    updated endpoint and display table for pcc / tint permit

--- src/constants/api-routes.js
+++ src/constants/api-routes.js
@@ -2,7 +2,7 @@
 export const LOGIN = '/login/signin';
 export const FIND_FILE = '/character-certificate-tint-permit/';
 export const POST_PCC_BIOMETRIC442_DATA = '/CharacterCertificate/biometrics442Print';
-export const POST_PCC_BIOMETRIC_DATA = '/CharacterCertificate/biometrics442Print';
+export const POST_PCC_BIOMETRIC_DATA = '/CharacterCertificate/biometrics';
 export const POST_TINT_PERMIT_BIOMETRIC_DATA = '/TintPermitApi/biometrics';
-export const POST_TINT_PERMIT_BIOMETRIC442_DATA = '/TintPermitApi/biometrics';
+export const POST_TINT_PERMIT_BIOMETRIC442_DATA = '/TintPermitApi/biometrics442Print';
 
