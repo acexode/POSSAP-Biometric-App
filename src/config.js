0d33e18ee483875ac06cf4234df827a30f800866
Sir Abubakar <abudawud92@gmail.com>
2024-05-28 14:58:31 +0100

    updated config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config ={
-    "ApiUrl1": "https://admin.possap.ng/api/v1/pss",
-    "ApiUrl": "http://test.possap.ng/api/v1/pss",
+    "ApiUrl": "https://admin.possap.ng/api/v1/pss",
+    "ApiUrl1": "http://test.possap.ng/api/v1/pss",
     // "ApiUrl": "http://pss.cbs/api/v1/pss",
     "BaseUrl": "http://localhost:3001",
     "middlewareApi": "https://possapbackend-development.ogtlprojects.com/api/v1"
