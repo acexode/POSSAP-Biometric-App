d74320c48b6ba6bc43b1d7f6aac426b2e301a7d2
amirdbt <ahmed.dambatta@outsourceglobal.com>
2025-12-02 09:32:58 +0100

    updt

--- src/config.js
+++ src/config.js
@@ -1,9 +1,8 @@
 {
     "ApiUrl1": "https://admin.possap.ng/api/v1/pss",
-    // "ApiUrl": "http://test.possap.ng/api/v1/pss",
+    "ApiUrl": "http://test.possap.ng/api/v1/pss",
     // "ApiUrl": "http://pss.cbs/api/v1/pss",
-    "BaseUrl": "https://possap-api-revamp.ogtlprojects.com",
-    "middlewareApi": "https://possap-api-revamp.ogtlprojects.com"
+    "BaseUrl": "http://localhost:3001",
+    "middlewareApi": "https://possapbackend-development.ogtlprojects.com/api/v1"
 
 };
-
