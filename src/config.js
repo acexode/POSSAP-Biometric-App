9e8edb851278857100755bdc99a91a300a1e6a8c
amirdbt <ahmed.dambatta@outsourceglobal.com>
2025-12-02 09:28:12 +0100

    in progress

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 {
     "ApiUrl1": "https://admin.possap.ng/api/v1/pss",
-    "ApiUrl": "http://test.possap.ng/api/v1/pss",
+    // "ApiUrl": "http://test.possap.ng/api/v1/pss",
     // "ApiUrl": "http://pss.cbs/api/v1/pss",
-    "BaseUrl": "http://localhost:3001",
-    "middlewareApi": "https://possapbackend-development.ogtlprojects.com/api/v1"
+    "BaseUrl": "https://possap-api-revamp.ogtlprojects.com",
+    "middlewareApi": "https://possap-api-revamp.ogtlprojects.com"
 
 };
 
--- src/utils/path-join.js
+++ src/utils/path-join.js
@@ -2,11 +2,11 @@ export default function pathJoin(parts, separator = '/') {
   return parts
     .map((part, index) => {
       if (index > 0) {
-        return part.replace(new RegExp(`^\\${separator}`), '');
+        return part?.replace(new RegExp(`^\\${separator}`), '');
       }
 
       if (index !== parts.length - 1) {
-        return part.replace(new RegExp(`\\${separator}$`), '');
+        return part?.replace(new RegExp(`\\${separator}$`), '');
       }
 
       return part;
